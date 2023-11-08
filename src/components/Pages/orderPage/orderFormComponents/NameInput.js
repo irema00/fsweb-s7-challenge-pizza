11d1685169a1b8f3c3cf849f16e34ae6bc9f0ed6
İrem Akın Tunga <iremakin00@gmail.com>
2023-11-08 21:57:38 +0300

    solves react dev tools autocomplete & id issues

--- src/components/Pages/orderPage/orderFormComponents/NameInput.js
+++ src/components/Pages/orderPage/orderFormComponents/NameInput.js
@@ -3,13 +3,13 @@ import "../PizzaForm.css";
 const NameInput = ({ onNameChange, error }) => {
   return (
     <div>
-      <label htmlFor="name">
+      <label htmlFor="name-input">
         <h4>Name</h4>
       </label>
       <input
         type="text"
         id="name-input"
-        autoComplete="name-input"
+        autoComplete="name"
         onChange={(e) => onNameChange(e.target.value)}
       />
       {error && <p style={{ color: "red" }}>{error}</p>}
