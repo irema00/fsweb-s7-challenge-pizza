cc038ab8a1a324f4c504024027a80d953cdd08c1
İrem Akın Tunga <iremakin00@gmail.com>
2023-11-08 20:12:58 +0300

    changes ingredientPrice to toppingsPrice

--- src/components/Pages/orderPage/orderFormComponents/TotalPrice.js
+++ src/components/Pages/orderPage/orderFormComponents/TotalPrice.js
@@ -1,13 +1,13 @@
 import React from "react";
 import "../PizzaForm.css";
-function TotalPrice({ basePrice, ingredientsPrice }) {
-  const totalPrice = basePrice + ingredientsPrice;
+function TotalPrice({ basePrice, toppingsPrice }) {
+  const totalPrice = basePrice + toppingsPrice;
 
   return (
     <>
       <h4>Order Total</h4>
       <div id="extra-total">
-        <p>Selections:</p> <p>{ingredientsPrice}₺ </p>
+        <p>Selections:</p> <p>{toppingsPrice}₺ </p>
       </div>
 
       <div id="total">
