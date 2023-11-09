c37f4c078d5433964d2f474d136467e17d9e7c62
İrem Akın Tunga <iremakin00@gmail.com>
2023-11-09 15:34:51 +0300

    Calculates total selections price

--- src/components/Pages/orderPage/orderFormComponents/TotalPrice.js
+++ src/components/Pages/orderPage/orderFormComponents/TotalPrice.js
@@ -1,13 +1,13 @@
 import React from "react";
 import "../PizzaForm.css";
-function TotalPrice({ basePrice, toppingsPrice }) {
-  const totalPrice = (basePrice + toppingsPrice).toFixed(2);
+function TotalPrice({ basePrice, toppingsPrice, count }) {
+  const totalPrice = (count * (basePrice + toppingsPrice)).toFixed(2);
 
   return (
     <>
       <h4>Order Total</h4>
       <div id="extra-total">
-        <p>Selections:</p> <p>{toppingsPrice}₺ </p>
+        <p>Selections:</p> <p>{toppingsPrice * count}₺ </p>
       </div>
 
       <div id="total">
