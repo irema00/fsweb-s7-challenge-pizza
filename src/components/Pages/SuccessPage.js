6e769df7f99dde077c0a44eab91cfe725dd261c7
İrem Akın Tunga <iremakin00@gmail.com>
2023-11-10 14:30:21 +0300

    improves success pg

--- src/components/Pages/SuccessPage.js
+++ src/components/Pages/SuccessPage.js
@@ -4,18 +4,43 @@ import "./SuccessPage.css";
 
 const Success = () => {
   const location = useLocation();
-
+  const { orderSummary } = location.state || {};
+  console.log("LOC CONTROL", location.state);
+  console.log("LOC CONTROL2", orderSummary);
   return (
     <>
-      <div className="success" id="success-page">
-        <div className="header">
-          <h3>Teknolojik Yemekler</h3>{" "}
-        </div>
+      <div className="success-container" id="success-page">
+        <h2 className="success-title">Teknolojik Yemekler</h2>
+        <div className="success-text"> lezzetin yolda</div>
+
         <div className="success-msg">
-          <h2>
-            TEBRIKLER <br />
-            SIPARISINIZ ALINDI!
-          </h2>
+          <h1>SIPARISINIZ ALINDI!</h1>
+        </div>
+        <div className="order-container">
+          <div className="order-summary">
+            <h2>{orderSummary.PizzaSelection}</h2>
+            <p>
+              Size: <span> {orderSummary.Size} </span>
+            </p>
+            <p>
+              Crust Type: <span> {orderSummary.CrustType}</span>
+            </p>
+            <p>
+              Toppings: <span> {orderSummary.Toppings}</span>
+            </p>
+          </div>
+          <div className="order-total-price">
+            <p>Order Total</p>
+
+            <div className="order-total2">
+              <p>Selections:</p>
+              <span> {orderSummary.SelectionTotal} ₺</span>
+            </div>
+            <div className="order-total2">
+              <p>Total:</p>
+              <span>{orderSummary.OrderTotal}₺</span>
+            </div>
+          </div>
         </div>
       </div>
     </>
